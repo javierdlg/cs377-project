ed054c5dcdd5b6e9f2ad18788c22bc1b3ee969fc
John Groves <jhgroves@cs.utexas.edu>
2015-11-22 13:59:11 -0600

    Finshed divideWork/bellmanFord/threadStart

--- RunGraph.cpp
+++ RunGraph.cpp
@@ -1,6 +1,7 @@
 #include "graph.h"
 #include <iostream>
 #include <vector>
+#include <climits>
 
 void loadGraph(istream& r){
 	// read DIMACS file and add nodes/edges to the allNodes vector
@@ -8,12 +9,24 @@ void loadGraph(istream& r){
 
 void divideWork(int threadNumber){
 	// initialize threadObjects, assign nodes
+	for(i = 0; i < threadNumber; ++i){
+		for(j = i; j<allNodes.size(); j+=threadNumber){
+			threads[i].inputNodes.push_back(allNodes[j]);
+		}
+	}
 }
 
 void bellmanFord(threadObject* thread){
+	
 	for(int i = 0; i < thread->inputNodes.size(); ++i){
-		
+		Node * node = thread->inputNodes[i];
+		for(int j = 0; j < node->input.size(); ++j){
+			Edge *edge = node->input[j];
+			if(edge->source->cost != INT_MAX && edge->source->cost < node->cost)
+				node->cost = edge->source->cost;
+		}		
 	}
+	pthread_barrier_wait(&barrier);
 }
 
 void threadStart(void* args){
@@ -27,10 +40,10 @@ int main(int args char* argv[]){
 	loadGraph(argv[1]);
 	divideWork(argv[2]);
 	// Start threads
-
+	pthread_barrier_init(&barrier, null, argv[2]);
 	for(int i = 0; i < argv[2]; ++i){
 		//pthread create
-		threadStart(&threads[i]);
+		pthread_create (&threads[i]->tid, NULL, threadStart, &threads[i]);
 	}
 
 	// pthread syncronize (barrier)

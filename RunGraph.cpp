4c8fcd404f5be577b7c835054aaed03aa012e104
John Groves <jhgroves@cs.utexas.edu>
2015-12-02 12:48:45 -0600

    working parallelized

--- RunGraph.cpp
+++ RunGraph.cpp
@@ -16,7 +16,7 @@ void loadGraph(const char* fileName){
 	ifstream in(fileName);
 	string line;
 	stringstream ss;
-	int maxNodes, maxEdges, nodeSource, nodeDestination, edge, weight;
+	int maxNodes, maxEdges, nodeSource, nodeDestination, weight;
 	while (getline(in, line)){
 		if(line.front() == 'c')
 			continue;
@@ -28,8 +28,7 @@ void loadGraph(const char* fileName){
 			getline(iss, s, ' ');
 			maxNodes = atoi(s.c_str());
 			getline(iss, s, ' ');
-			maxEdges = atoi(s.c_str());
-			printf("test %d\n", maxNodes);
+			//maxEdges = atoi(s.c_str());
 			allNodes.reserve(maxNodes);
 			for(int i = 0; i < maxNodes; ++i)
 			{
@@ -46,14 +45,14 @@ void loadGraph(const char* fileName){
 			string s;
 			getline(iss, s, ' ');
 			getline(iss, s, ' ');
+			nodeSource = atoi(s.c_str()) - 1;
+			getline(iss, s, ' ');
+			nodeDestination = atoi(s.c_str()) - 1;
+			getline(iss, s, ' ');
 			weight = atoi(s.c_str());
-			getline(iss, s, ' ');
-			nodeSource = atoi(s.c_str());
-			getline(iss, s, ' ');
-			nodeDestination = atoi(s.c_str());
 			//ss >> weight >> nodeDestination >> nodeSource;
-			Edge e(allNodes[nodeSource], allNodes[nodeDestination], weight);
-			allNodes[nodeDestination]->addEdge(&e);
+			Edge *e = new Edge(allNodes[nodeSource], allNodes[nodeDestination], weight);
+			allNodes[nodeDestination]->addEdge(e);
 			ss.str(string());
 		}
 	}
@@ -66,10 +65,14 @@ void divideWork(int threadNumber){
 	for(int i = 0; i < threadNumber; ++i){
 		threadObject thread;
 		threads.push_back(thread);
+		printf("push back %d\n", i);
 	}
 	for(int i = 0; i < threadNumber; ++i){
 		for(unsigned int j = i; j < allNodes.size(); j += threadNumber){
 			threads[i].inputNodes.push_back(allNodes[j]);
+			for(unsigned int k = 0; k < allNodes[j]->input.size(); ++k){
+				Edge * e = allNodes[j]->input[k];
+			}
 		}
 	}
 	for (unsigned int i = allNodes.size() - (allNodes.size()%threadNumber); i < allNodes.size(); ++i){
@@ -84,7 +87,9 @@ void bellmanFord(threadObject* thread){
 		for(unsigned int j = 0; j < node->input.size(); ++j){
 			Edge *edge = node->input[j];
 			if(edge->source->cost != INT_MAX && (edge->source->cost + edge->weight) < node->cost)
+			{
 				node->cost = edge->source->cost + edge->weight;
+			}
 		}		
 	}
 	pthread_barrier_wait(&barrier);
@@ -92,7 +97,7 @@ void bellmanFord(threadObject* thread){
 
 void* threadStart(void* args){
 	threadObject* args1 = (threadObject*)args;
-	for(unsigned int i = 0; i < (allNodes.size() - 1)/threadCount; ++i){
+	for(unsigned int i = 0; i < (allNodes.size() - 1); ++i){
 		bellmanFord(args1);
 	}
 	return args;
@@ -100,7 +105,7 @@ void* threadStart(void* args){
 
 void graphPrint(){
 	for(unsigned int i = 0; i < allNodes.size(); ++i){
-		cout << allNodes[i]->cost;
+		cout << allNodes[i]->cost <<endl;
 	}
 }
 
@@ -114,7 +119,7 @@ int main(int args, char* argv[]){
 	loadGraph(argv[1]);
 
 	divideWork(threadCount);
-
+	allNodes[0]->cost = 0;
 	// Start threads
 	pthread_barrier_init(&barrier, NULL, threadCount);
 	for(int i = 0; i < threadCount; ++i){
@@ -122,6 +127,7 @@ int main(int args, char* argv[]){
 		pthread_create (&(threads[i].tid), NULL, threadStart, (void *)(&threads[i]));
 	}
 	printf(" Super test %d\n",2);
+
 	// pthread syncronize (barrier)
 	for(int i = 0; i < threadCount; ++i){
 		pthread_join(threads[i].tid, NULL);
